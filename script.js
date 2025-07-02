433a327cb8e7c5c4dfe7bfce3ef03655af0654e1
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-02 16:37:56 +0200

    else_if_per3

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
 // comando il numero dall 1 al 100
-for (let i = 1; i <100; i++) {
-    if (i % 3 === 0 && i% 5 === 0 )
-     console.log("FizzBuzz");
-}
+for (let i = 1; i <100; i++) 
+    if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+  // solo divisibile per 3
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+ //solo divisibile per 5
+
+ else if ((i % 5 === 0)) {
+    console.log ()
+    
+ } else {
+    
+ }
